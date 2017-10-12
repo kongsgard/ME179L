ba7b340398c0e1e0d5773d11906406c0a744e111
jfanchiang <32745734+jfanchiang@users.noreply.github.com>
2017-10-12 10:56:50 -0700

    Add switch pin to control settings phase

--- WallFollower/WallFollower.ino
+++ WallFollower/WallFollower.ino
@@ -3,8 +3,8 @@ two Analog inputs and serial output
 */
 
 
-// These constants won't change.  They're used to give names
-// to the pins used:
+// Pin constants:
+#define switchPin       11
 
 const int analogInPinL = A0;  // Analog input from left reflector
 const int analogInPinR = A1; // Analog input from right reflector
@@ -14,24 +14,35 @@ int sensorValueR = 0;        // value read on right
 
 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
+  // Setup switch:
+  pinMode(switchPin, INPUT);
+  digitalWrite(switchPin, HIGH);
+
+  // Initialize serial communications at 9600 bps:
+  Serial.begin(9600);
 }
 
 void loop() {
+  while (digitalRead(switchPin))
+  {
+    // Wait until switch is pressed.
+    // Change settings here
+    delay(150);
+  }
+
   // read the analog on the left:
-  sensorValueL = analogRead(analogInPinL);            
+  sensorValueL = analogRead(analogInPinL);
   // read the analog value on the right:
-  sensorValueR = analogRead(analogInPinR); 
+  sensorValueR = analogRead(analogInPinR);
   // print the results to the serial monitor:
-  
-  Serial.print("sensorL = " );                       
-  Serial.print(sensorValueL);     
-  Serial.print(" sensorR= ");      
+
+  Serial.print("sensorL = " );
+  Serial.print(sensorValueL);
+  Serial.print(" sensorR = ");
   Serial.println(sensorValueR);
 
   // wait 10 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(10);                     
+  delay(10);
 }
