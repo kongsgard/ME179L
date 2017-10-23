489ccaaa73ee2f784c30d6054936257aee010e6a
Sondre <sondre.kongsgaard@gmail.com>
2017-10-23 10:13:55 -0700

    Add first working tuning of P-controller

--- WallFollower/WallFollower.ino
+++ WallFollower/WallFollower.ino
@@ -4,7 +4,7 @@
 #include <PID_v1.h>
 #include <SoftwareSerial.h>
 
-//#define DEBUG           1
+#define DEBUG           1
 
 // Pin constants:
 #define switchPin       11
@@ -19,7 +19,7 @@ AF_DCMotor Right_Motor(RightMotorPin, MOTOR34_1KHZ); // Set up right motor on po
 
 // P controller control variables
 double input, output;
-int desiredSensorValue = 300;
+int desiredSensorValue = 200;
 double scaledOutput    = 1;
 int fastMotorSpeed     = 150;
 int slowMotorSpeed     = 150;
@@ -58,24 +58,37 @@ void loop() {
   sensorValueLong = analogRead(analogInPinLong);
   input = abs(sensorValueShort - desiredSensorValue);
 
-  slowMotorSpeed = constrain(SPEED - 100, 100, 250);
-  fastMotorSpeed = constrain(SPEED + 50, 150, 250);
 
-  Serial.print(slowMotorSpeed);
-  Serial.print(" ");
-  Serial.println(fastMotorSpeed);
+
+  mySerial.print("?x00?y1");
+  mySerial.print("L: ");
+  mySerial.print(sensorValueShort);
+
+  //Serial.print(slowMotorSpeed);
+  //Serial.print(" ");
+  //Serial.println(fastMotorSpeed);
 
   // Slight turn
   if (sensorValueShort > desiredSensorValue)
   {
+    slowMotorSpeed = constrain(150, 100, 250);
+    fastMotorSpeed = constrain(SPEED + input, 150, 250);
+
     // Too close to the wall - turn left
     Right_Motor.setSpeed(fastMotorSpeed);
     Left_Motor.setSpeed(slowMotorSpeed);
-    DriveForward();
+
+    Right_Motor.run(FORWARD);
+    Left_Motor.run(BACKWARD);
+
+    //DriveForward();
   }
   else
   {
     // Too far from the wall - turn right
+    slowMotorSpeed = constrain(SPEED - 2 * input, 100, 250);
+    fastMotorSpeed = constrain(SPEED + input, 150, 250);
+
     Right_Motor.setSpeed(slowMotorSpeed);
     Left_Motor.setSpeed(fastMotorSpeed);
     DriveForward();
@@ -141,15 +154,15 @@ void printDebug()
   //Serial.print(" sensorLong = ");
   //Serial.println(sensorValueLong);
 
-  Serial.print("Input: ");
-  Serial.println(input);
-  Serial.print("PID output: ");
-  Serial.println(output);
-  Serial.print("Scaled output: ");
-  Serial.println(scaledOutput);
+  //Serial.print("Input: ");
+  //Serial.println(input);
+  //Serial.print("PID output: ");
+  //Serial.println(output);
+  //Serial.print("Scaled output: ");
+  //Serial.println(scaledOutput);
 
-  mySerial.print("?x00?y1");
-  mySerial.print("output: ");
-  mySerial.print(scaledOutput);
+  //mySerial.print("?x00?y1");
+  //mySerial.print("output: ");
+  //mySerial.print(scaledOutput);
   #endif
 }
