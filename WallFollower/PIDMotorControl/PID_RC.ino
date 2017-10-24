8df1dbbb549941ea922d45b9d40cf50a3de70a7b
jfanchiang <32745734+jfanchiang@users.noreply.github.com>
2017-10-23 17:22:22 -0700

    changed to PID
    assignment requirement

--- WallFollower/PIDMotorControl/PID_RC.ino
+++ WallFollower/PIDMotorControl/PID_RC.ino
@@ -1,77 +1,83 @@
 // Include encoder and PID libraries
-#include <Encoder.h>
+#include <AFMotor.h>
 #include <PID_v1.h>
+#include <SoftwareSerial.h>
 
 // Define pins
-Encoder Enc_1(6, 16);
-int Motor_1 = 9;
-int Dir_1 = 13;
-int Trans_Pin = 12;
-int Rudder_Pin = 14;
+#define LeftMotorPin  3
+#define RightMotorPin  4
+#define LCDTxPin     13
+#define rxPin          6
 
-// Initialize PWM inputs
-int Trans_PWM;
-int Rudder_PWM;
+
+const int ShortIR = A0;
+const int LongIR = A1;
+
+int sensorValueShort = 0;     // Short range sensor value
+int sensorValueLong = 0;
 
 // Define PID parameters
 double Setpoint, Input, Output;
 double Kp = 0.7;
-double Ki = 0.01;
-double Kd = 0.015;
+double Ki = 0.05;
+double Kd = 0.15;
 PID PID_1(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+AF_DCMotor Left_Motor(LeftMotorPin, MOTOR34_1KHZ); // Set up left motor on port 4, 1KHz pwm
+AF_DCMotor Right_Motor(RightMotorPin, MOTOR34_1KHZ); // Set up right motor on port 3, 1KHz pwm
+SoftwareSerial mySerial = SoftwareSerial(rxPin, LCDTxPin);
 
 void setup()
 {
   // Set pin modes
-  pinMode(Motor_1, OUTPUT);
-  pinMode(Dir_1, OUTPUT);
-  pinMode(Trans_Pin, INPUT);
+  pinMode(LeftMotorPin, OUTPUT);
+  pinMode(RightMotorPin,OUTPUT);
+  pinMode(ShortIR, INPUT);
 
   // Set initial positions
-  Input = Enc_1.read();
-  Setpoint = 0.5837*pulseIn(Trans_Pin, HIGH) + 1095;
+  Setpoint = 300;
 
   // Set PID limits and mode
-  PID_1.SetOutputLimits(-255,255);
   PID_1.SetMode(AUTOMATIC);
 
   // Open serial port
   Serial.begin(9600);
 }
 
-// Store last inputs
-long oldPosition = Input;
 
 void loop()
 {
   // Read transition switch and rudder stick
-  Trans_PWM = pulseIn(Trans_Pin, HIGH);
-  Rudder_PWM = pulseIn(Rudder_Pin, HIGH);
-
-  // Mix and compute desired motor position
-  Setpoint = (72963*pulseIn(Trans_Pin, HIGH)/125000 + 1095)+(Rudder_PWM-1500)*0.6;
-
-  // Read encoder
-  Input = Enc_1.read();
-
-  // Update serial monitor with encoder information
-  if (Input != oldPosition)
-  {
-    Serial.println(Setpoint-Input);
-    oldPosition = Input;
-  }
+  sensorValueShort = analogRead(ShortIR)
+  diff = sensorValueShort - Setpoint;
+  Input = abs(sensorValueShort - Setpoint);
 
   // Run PID controller
   PID_1.Compute();
 
+  Serial.print("Output: ");
+  Serial.println(Output);
+
   // Apply proper motor direction to output
-  if (Output >= 0 && Output <= 255)
-  {
-    analogWrite(Motor_1, Output);
-    digitalWrite(Dir_1, 0);
+  if (diff > 0 && diff <= 50){
+    Right_Motor.setSpeed(Output);
+    Left_Motor.setSpeed(0.6*Output);
   }
-  else if (Output >= -255 && Output < 0)
-  {
-    analogWrite(Motor_1, -1*Output);
-    digitalWrite(Dir_1, 1);
+  else if (diff < 0 && diff >= -50){
+    Right_Motor.setSpeed(0.6*Output);
+    Left_Motor.setSpeed(Output);
   }
+  else if (diff > 50){
+    Right_Motor.setSpeed(Output);
+    Left_Motor.setSpeed(0.3*Output);
+  }
+  else if (diff < -50){
+    Right_Motor.setSpeed(0.3*Output);
+    Left_Motor.setSpeed(Output);
+  }
+  else {
+    Right_Motor.setSpeed(Output);
+    Left_Motor.setSpeed(Output);
+  }
+
+}
